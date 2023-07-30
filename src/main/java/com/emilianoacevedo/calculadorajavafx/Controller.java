2d2625fc6810ce0f183fd30f1a06bc2b99f14c7f
darthenis <emi.acevedo.letras@gmail.com>
2023-07-30 15:12:01 -0300

    added buttons and number functions

---
+++ src/main/java/com/emilianoacevedo/calculadorajavafx/Controller.java
@@ -0,0 +1,17 @@
+package com.emilianoacevedo.calculadorajavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    void addValue(ActionEvent event) {
+        textField.setText(textField.getText() + ((Button)event.getSource()).getText());
+    }
+}
